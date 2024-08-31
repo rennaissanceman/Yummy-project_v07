268a1487ea8dfd62604f5a5cf23abed7df27da45
Robert <szinzon@gmail.com>
2024-08-31 14:22:43 +0200

    commit-04 Configuration

--- src/main/java/pl/yummy/infrastructure/configuration/HibernateUtil.java
+++ src/main/java/pl/yummy/infrastructure/configuration/HibernateUtil.java
@@ -1,4 +1,23 @@
 package pl.yummy.infrastructure.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.cfg.Environment;
+
+import java.util.Map;
+
+@Slf4j
 public class HibernateUtil {
+
+    private static final Map<String, Object> HIBERNATE_SETTINGS = Map.ofEntries(
+            Map.entry(Environment.DRIVER, "org.postgresql.Driver"),
+            Map.entry(Environment.URL, "jdbc:postgresql://localhost:5432/yummy"),
+            Map.entry(Environment.USER, "postgres"),
+            Map.entry(Environment.PASS, "postgres"),
+            Map.entry(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect"),
+            Map.entry(Environment.CONNECTION_PROVIDER, "org.hibernate.hikaricp.internal.HikariCPConnectionProvider"),
+            Map.entry(Environment.HBM2DDL_AUTO, "validate"),
+            Map.entry(Environment.SHOW_SQL, true),
+            Map.entry(Environment.FORMAT_SQL, false)
+    );
+
 }
