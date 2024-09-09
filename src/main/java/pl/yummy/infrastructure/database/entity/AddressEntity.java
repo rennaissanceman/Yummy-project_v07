739123748cbe94e3d52a96f5036334a365123f7d
Robert <szinzon@gmail.com>
2024-09-09 22:46:38 +0200

    commit-13 Entity v.7

--- src/main/java/pl/yummy/infrastructure/database/entity/AddressEntity.java
+++ src/main/java/pl/yummy/infrastructure/database/entity/AddressEntity.java
@@ -1,12 +1,47 @@
 package pl.yummy.infrastructure.database.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "addressId")
+@ToString(of = {"addressId", "country", "city", "postalCode", "street", "streetNumber"})
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "address")
 public class AddressEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
     private Integer addressId;
+
+    @Column(name = "country")
     private String country;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "postal_code")
     private String postalCode;
+
+    @Column(name = "street")
     private String street;
+
+    @Column(name = "street_number")
     private String streetNumber;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "address")
+    private CustomerEntity customer;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "address")
+    private OrderEntity order;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "address")
+    private RestaurantEntity restaurant;
+
 
 }
