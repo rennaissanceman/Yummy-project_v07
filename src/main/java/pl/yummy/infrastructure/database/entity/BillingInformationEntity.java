83c1e7a118beaba07c2d9304a14759501af993cd
Robert <szinzon@gmail.com>
2024-09-09 14:14:28 +0200

    commit-11 Entity v.5

--- src/main/java/pl/yummy/infrastructure/database/entity/BillingInformationEntity.java
+++ src/main/java/pl/yummy/infrastructure/database/entity/BillingInformationEntity.java
@@ -4,4 +4,10 @@ public class BillingInformationEntity {
 
     private Integer billingInformationId;
     private CustomerEntity customer;
+    private String companyName;
+    private String vatNumber;
+    private String companyAddress;
+    private String city;
+    private String postalCode;
+    private String country;
 }
--- src/main/java/pl/yummy/infrastructure/database/entity/CustomerEntity.java
+++ src/main/java/pl/yummy/infrastructure/database/entity/CustomerEntity.java
@@ -2,5 +2,21 @@ package pl.yummy.infrastructure.database.entity;
 
 public class CustomerEntity {
 
+    private Integer customerId;
+    private String customerType;
+    private String email;
+    private String phoneNumber;
+    private AddressEntity address;
+    private String name;
+    private String surname;
+    private String companyName;
+    private String createdAt;
+    private String updatedAt;
+
+
+    private OrderEntity order;
+    private DeliveryEntity delivery;
+    private InvoiceEntity invoice;
+    private BillingInformationEntity billingInformation;
 
 }
