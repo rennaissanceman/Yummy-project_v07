b710ddd105f8c82067707af7541206600187a031
Robert <szinzon@gmail.com>
2024-09-18 23:58:14 +0200

    commit-20 New Entities-2

--- src/main/java/pl/yummy/infrastructure/database/entity/AddressEntity.java
+++ src/main/java/pl/yummy/infrastructure/database/entity/AddressEntity.java
@@ -16,26 +16,30 @@ public class AddressEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "address_id")
+    @Column(name = "address_id", nullable = false)
     private Integer addressId;
 
-    @Column(name = "country")
+    @Column(name = "country", nullable = false)
     private String country;
 
-    @Column(name = "city")
+    @Column(name = "city", nullable = false)
     private String city;
 
-    @Column(name = "postal_code")
+    @Column(name = "postal_code", nullable = false)
     private String postalCode;
 
-    @Column(name = "street")
+    @Column(name = "street", nullable = false)
     private String street;
 
-    @Column(name = "street_number")
+    @Column(name = "street_number", nullable = false)
     private String streetNumber;
 
+    @OneToOne(fetch = FetchType.EAGER, mappedBy = "address")
+    private DeliveryAddressEntity deliveryAddress;
 
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "address")
+    private RestaurantEntity restaurant;
 
-
-
+    @OneToOne(fetch = FetchType.EAGER, mappedBy = "address")
+    private BillingInformationEntity billingInformation;
 }
