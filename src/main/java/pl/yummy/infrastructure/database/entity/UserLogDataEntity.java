f76c86bdeae2ddda27ed3bd1c75b889ad09d83ef
Robert <szinzon@gmail.com>
2024-11-12 00:16:55 +0100

    commit-25 New Entities-6c

--- src/main/java/pl/yummy/infrastructure/database/entity/UserLogDataEntity.java
+++ src/main/java/pl/yummy/infrastructure/database/entity/UserLogDataEntity.java
@@ -20,7 +20,7 @@ public class UserLogDataEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_log_data_id")
-    private Integer userLogDataId;
+    private Integer userLogDataIdd;
 
     @Column(name = "password_hash")
     private String passwordHash;
